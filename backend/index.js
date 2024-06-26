2745ff1100148f594207c526e10e736479fb9eea
Aditya-M02 <adityamah2002@gmail.com>
2024-06-27 00:50:05 +0530

    comments

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,19 @@ So basically this is the root file of the backend on top of which all routes are
 */
 
 // ----------------
+
+// this is basically the require module syntax.
 const express = require("express");
 
+// You can also do
+// import express from "express"
 
 
 // cors is basically used to connect backend and frontend, as we are using different urls for both
 const cors = require("cors");
 const rootRouter = require("./routes/index");
 
+// this "app" thing is like having Math. , basically you can use many things in that.
 const app = express();
 
 
@@ -27,5 +32,5 @@ app.use(express.json());
 // So here, any request starting with "/api/v1", that request will be handled by the rootRouter in the ./routes/index file.
 app.use("/api/v1", rootRouter);
 
-
+// sab kaam hi ports pr hota ha.
 app.listen(3000);
